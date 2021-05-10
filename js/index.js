9e6924163c427feb4c578590e0bc1d82c0d1adf4
mechalisk1357 <33808695+mechalisk1357@users.noreply.github.com>
2021-05-10 14:35:52 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 var stakedCPU = 0
 
 const round = (num, places) => {
-	return Math.round(num * Math.pow(10, places)) / Math.pow(10, places)
+  return Math.round(num * Math.pow(10, places)) / Math.pow(10, places)
 }
 
 const getCPU = (accountData) => {
@@ -11,37 +11,49 @@ const getCPU = (accountData) => {
   }).then(res => {
     return res.json()
   }).then(data => {
-    var available = (data.cpu_limit.used/data.cpu_limit.max)*100
+    var available = (data.cpu_limit.used / data.cpu_limit.max) * 100
     stakedCPU = parseFloat(data.self_delegated_bandwidth.cpu_weight.split(" ")[0])
     let cpuPercent = round(available, 2)
     $("div#cpu").text(cpuPercent + "%")
-   	
+
     if (cpuPercent >= 100) {
-    	$("div#myBar").css("background-color", "red")
+      $("div#myBar").css("background-color", "red")
     } else {
-    	$("div#myBar").css("background-color", "green")
+      $("div#myBar").css("background-color", "green")
     }
     $("div#myBar").css("width", `${cpuPercent >= 100 ? 100 : cpuPercent}%`)
   })
 }
 
 const getTokens = (accountData) => {
-	fetch(`https://eosauthority.com/api/spa/account/${accountData.account_name}/tokens?network=wax`, {
-  	method: "GET"
+  fetch(`https://eosauthority.com/api/spa/account/${accountData.account_name}/tokens?network=wax`, {
+    method: "GET"
   }).then(res => {
-  	return res.json()
+    return res.json()
   }).then(tokenData => {
-  		let swapPriceData = {"json":true,"code":"alcorammswap","scope":"alcorammswap","table":"pairs","lower_bound":"","upper_bound":"","index_position":1,"key_type":"","limit":1000,"reverse":false,"show_payer":false}
-      fetch("https://wax.greymass.com/v1/chain/get_table_rows", {
-        method: "POST",
-        body: JSON.stringify(swapPriceData)
-      }).then(res => {
-        return res.json()
-      }).then(data => {
-				var totalWax = 0
+    let swapPriceData = {
+      "json": true,
+      "code": "alcorammswap",
+      "scope": "alcorammswap",
+      "table": "pairs",
+      "lower_bound": "",
+      "upper_bound": "",
+      "index_position": 1,
+      "key_type": "",
+      "limit": 1000,
+      "reverse": false,
+      "show_payer": false
+    }
+    fetch("https://wax.greymass.com/v1/chain/get_table_rows", {
+      method: "POST",
+      body: JSON.stringify(swapPriceData)
+    }).then(res => {
+      return res.json()
+    }).then(data => {
+      var totalWax = 0
 
-        $("table#tokens").empty()
-        $("table#tokens").append(`
+      $("table#tokens").empty()
+      $("table#tokens").append(`
         	<tr>
             <th>Symbol</th>
             <th>Amount</th>
@@ -49,41 +61,44 @@ const getTokens = (accountData) => {
             <th>Total Wax</th>
           </tr>
    			`)
-        tokenData.forEach((token) => {
+      tokenData.forEach((token) => {
+        console.log(token)
+        let convData = data.rows.find(row => row.pool2.quantity.includes(token.symbol) || row.pool1.quantity.includes(token.symbol))
+        console.log(convData)
+        let waxPool = convData.pool1.quantity.includes("WAX") ? convData.pool1 : convData.pool2
+        let tokenPool = convData.pool1.quantity.includes(token.symbol) ? convData.pool1 : convData.pool2
+        let waxQuantity = parseFloat(waxPool.quantity.split(" ")[0])
+        let tokenQuantity = parseFloat(tokenPool.quantity.split(" ")[0])
+        let tokenToWax = waxQuantity / tokenQuantity
 
-        	let convData = data.rows.find(row => row.pool2.quantity.includes(token.symbol))
-          let waxQuantity = parseFloat(convData.pool1.quantity.split(" ")[0])
-          let tokenQuantity = parseFloat(convData.pool2.quantity.split(" ")[0])
-          let tokenToWax = waxQuantity/tokenQuantity
 
-
-          if (token.symbol === "WAX") {
-          	totalWax += stakedCPU
-            $("table#tokens").append(`<tr><td>${token.symbol}</td><td>${token.amount}</td><td>1</td><td>${token.amount}</td></tr>`)
-          } else {
-          	let estWax =round(token.amount * tokenToWax, 3)
-            totalWax += estWax
-            $("table#tokens").append(`<tr><td>${token.symbol}</td><td>${token.amount}</td><td>${round(tokenToWax, 3)}</td><td>${round(estWax, 3)}</td></tr>`)
-          }
-        })
-        $("table#tokens").append(`<tr><td>Staked</td><td></td><td>1</td><td>${stakedCPU}</td></tr>`)
-        $("table#tokens").append(`<tr><td>Total</td><td></td><td></td><td>${totalWax}</td></tr>`)
-        $("#data").removeClass("hidden")
+        if (token.symbol === "WAX") {
+          totalWax += stakedCPU
+          $("table#tokens").append(`<tr><td>${token.symbol}</td><td>${token.amount}</td><td>1</td><td>${token.amount}</td></tr>`)
+        } else {
+          let estWax = round(token.amount * tokenToWax, 3)
+          totalWax += estWax
+          $("table#tokens").append(`<tr><td>${token.symbol}</td><td>${token.amount}</td><td>${round(tokenToWax, 3)}</td><td>${round(estWax, 3)}</td></tr>`)
+        }
       })
+      $("table#tokens").append(`<tr><td>Staked</td><td></td><td>1</td><td>${stakedCPU}</td></tr>`)
+      $("table#tokens").append(`<tr><td>Total</td><td></td><td></td><td>${totalWax}</td></tr>`)
+      $("#data").removeClass("hidden")
+    })
   })
 }
 
 var intervalId;
 
 const stopPolling = () => {
-  	console.log("Clearing Interval", intervalId)
-  	clearInterval(intervalId)
+  console.log("Clearing Interval", intervalId)
+  clearInterval(intervalId)
 }
 
 const submitAccountAddress = () => {
-	if (intervalId) {
-  	console.log("Clearing Interval", intervalId)
-  	clearInterval(intervalId)
+  if (intervalId) {
+    console.log("Clearing Interval", intervalId)
+    clearInterval(intervalId)
   }
 
   let data = {
